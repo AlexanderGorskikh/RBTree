0d1e7dd93b6522b5e26412cd30ba8fa2d4d284e0
AlexanderGorsc <alexandergorsc@gmail.com>
2023-08-19 19:02:21 +0300

    Initial file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+//        final RBTree<Integer> testTree= new RBTree<>();
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+//            while (true){
+//                int key = Integer.parseInt(reader.readLine());
+//                testTree.add(key);
+//                System.out.println("finish");
+//            }
+//        } catch (Exception e){
+//
+//        }
+    }
+}
