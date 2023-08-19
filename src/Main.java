5ac3abd3eb3000d5c3f2eb22f0d091716d00a9a4
AlexanderGorsc <alexandergorsc@gmail.com>
2023-08-19 20:48:38 +0300

    Добавил метод вывода дерева на экран (в порядке увеличения)

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-//        final RBTree<Integer> testTree= new RBTree<>();
-//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-//            while (true){
-//                int key = Integer.parseInt(reader.readLine());
-//                testTree.add(key);
-//                System.out.println("finish");
-//            }
-//        } catch (Exception e){
+	public static void main(String[] args) {
+		//		final RBTree testTree = new RBTree();
+//		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+//			while (true) {
+//				int key = Integer.parseInt(reader.readLine());
+//				testTree.add(key);
+//				System.out.println("finish");
+//			}
+//		} catch (Exception e) {
 //
-//        }
-    }
+//		}
+		RBTree tree = new RBTree();
+		tree.add(5);
+		tree.add(7);
+		tree.add(10);
+		tree.add(2);
+		tree.printTree();
+	}
 }
--- src/RBTree.java
+++ src/RBTree.java
@@ -1,121 +1,132 @@
+import javax.lang.model.element.NestingKind;
 
 public class RBTree {
-    /**
-     * Красно-черное дерево
-     * Корень всегда черный
-     * Новый элемент по умолчанию красный (для частного левостороннего дерева красный элемент слева)
-     * Все дети красного узла должны быть черные
-     * Все листья будут черными и на уровне null
-     * Цвет нужен для алгоритма балансировки дерева
-     */
+	/**
+	 * Красно-черное дерево Корень всегда черный Новый элемент по умолчанию красный
+	 * (для частного левостороннего дерева красный элемент слева) Все дети красного
+	 * узла должны быть черные Все листья будут черными и на уровне null Цвет нужен
+	 * для алгоритма балансировки дерева
+	 */
 
-    private Node root;
+	private Node root;
 
-    private Node LSwap(Node node) {
-        Node leftChild = node.leftChild;
-        Node betweenChild = leftChild.rightChild; // элемент который меняет своих родителей
-        leftChild.rightChild = node;
-        node.leftChild = betweenChild;
-        leftChild.color = node.color;
-        node.color = Color.RED;
-        return leftChild;
-    }
+	private Node LSwap(Node node) {
+		Node leftChild = node.leftChild;
+		Node betweenChild = leftChild.rightChild; // элемент который меняет своих родителей
+		leftChild.rightChild = node;
+		node.leftChild = betweenChild;
+		leftChild.color = node.color;
+		node.color = Color.RED;
+		return leftChild;
+	}
 
-    private Node RSwap(Node node) {
-        Node rightChild = node.rightChild;
-        Node betweenChild = rightChild.leftChild;
-        rightChild.leftChild = node;
-        node.rightChild = betweenChild;
-        rightChild.color = node.color;
-        node.color = Color.RED;
-        return rightChild;
-    }
+	private Node RSwap(Node node) {
+		Node rightChild = node.rightChild;
+		Node betweenChild = rightChild.leftChild;
+		rightChild.leftChild = node;
+		node.rightChild = betweenChild;
+		rightChild.color = node.color;
+		node.color = Color.RED;
+		return rightChild;
+	}
 
-    private void colorSwap(Node node) { // Возникает в особой ситуации, когда оба ребенка красные
-        node.rightChild.color = Color.BLACK;
-        node.leftChild.color = Color.BLACK;
-        node.color = Color.RED;
-    }
+	private void colorSwap(Node node) { // Возникает в особой ситуации, когда оба ребенка красные
+		node.rightChild.color = Color.BLACK;
+		node.leftChild.color = Color.BLACK;
+		node.color = Color.RED;
+	}
 
-    public boolean add(int key) {
-        if (root != null) {
-            boolean result = addNode(root, key);
-            root = rebalanced(root);
-            root.color = Color.BLACK;
-            return result;
-        } else {
-            root = new Node();
-            root.color = Color.BLACK;
-            root.key = key;
+	public boolean add(int key) {
+		if (root != null) {
+			boolean result = addNode(root, key);
+			root = rebalanced(root);
+			root.color = Color.BLACK;
+			return result;
+		} else {
+			root = new Node();
+			root.color = Color.BLACK;
+			root.key = key;
+			return true;
+		}
+	}
 
-            return true;
-        }
-    }
+	private boolean addNode(Node node, int key) {
+		if (key == node.key) { // Исключаем дублирование элементов
+			return false;
+		} else {
+			if (key < node.key) {
+				if (node.leftChild != null) {
+					boolean result = addNode(node.leftChild, key);
+					node.leftChild = rebalanced(node.leftChild);
+					return result;
+				} else {
+					node.leftChild = new Node();
+					node.leftChild.color = Color.RED;
+					node.leftChild.key = key;
+					return true;
+				}
+			} else {
+				if (node.rightChild != null) {
+					boolean result = addNode(node.rightChild, key);
+					node.rightChild = rebalanced(node.rightChild);
+					return result;
+				} else {
+					node.rightChild = new Node();
+					node.rightChild.color = Color.RED;
+					node.rightChild.key = key;
+					return true;
+				}
 
-    private boolean addNode(Node node, int key) {
-        if (key == node.key) { // Исключаем дублирование элементов
-            return false;
-        } else {
-            if (key < node.key) {
-                if (node.leftChild != null) {
-                    boolean result = addNode(node.leftChild, key);
-                    node.leftChild = rebalanced(node.leftChild);
-                    return result;
-                } else {
-                    node.leftChild = new Node();
-                    node.leftChild.color = Color.RED;
-                    node.leftChild.key = key;
-                    return true;
-                }
-            } else {
-                if (node.rightChild != null) {
-                    boolean result = addNode(node.rightChild, key);
-                    node.rightChild = rebalanced(node.rightChild);
-                    return result;
-                } else {
-                    node.rightChild = new Node();
-                    node.rightChild.color = Color.RED;
-                    node.rightChild.key = key;
-                    return true;
-                }
+			}
+		}
+	}
 
-            }
-        }
-    }
+	private Node rebalanced(Node node) {
+		Node result = node;
+		boolean needRebalance;
+		do {
+			needRebalance = false;
+			if (result.rightChild != null && result.rightChild.color == Color.RED
+					&& (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
+				needRebalance = true;
+				result = RSwap(result);
+			}
+			if (result.leftChild != null && result.leftChild.color == Color.RED && result.leftChild.leftChild != null
+					&& result.leftChild.leftChild.color == Color.RED) {
+				needRebalance = true;
+				result = LSwap(result);
+			}
+			if (result.leftChild != null && result.leftChild.color == Color.RED && result.rightChild != null
+					&& result.rightChild.color == Color.RED) {
+				needRebalance = true;
+				colorSwap(result);
+			}
+		} while (needRebalance);
+		return result;
+	}
 
-    private Node rebalanced(Node node) {
-        Node result = node;
-        boolean needRebalance;
-        do {
-            needRebalance = false;
-            if (result.rightChild != null && result.rightChild.color == Color.RED &&
-                    (result.leftChild == null || result.leftChild.color == Color.BLACK)) {
-                needRebalance = true;
-                result = RSwap(result);
-            }
-            if (result.leftChild != null && result.leftChild.color == Color.RED &&
-                    result.leftChild.leftChild != null && result.leftChild.leftChild.color == Color.RED) {
-                needRebalance = true;
-                result = LSwap(result);
-            }
-            if (result.leftChild != null && result.leftChild.color == Color.RED &&
-                    result.rightChild != null && result.rightChild.color == Color.RED) {
-                needRebalance = true;
-                colorSwap(result);
-            }
-        } while (needRebalance);
-        return result;
-    }
+	public void printTree() {
+		printTree(root);
+	}
 
+	private void printTree(Node node) {
+		if (node.leftChild != null) {
+			printTree(node.leftChild);
+		}
+		System.out.print(node.key + " ");
+		if (node.rightChild != null) {
+			printTree(node.rightChild);
+		}
+	}
 
-    private class Node {
-        int key;
-        Node leftChild;
-        Node rightChild;
-        Color color;
-    }
+	private class Node {
+		int key;
+		Node leftChild;
+		Node rightChild;
+		Color color;
+	}
 
-    enum Color {
-        RED, BLACK
-    }
+	enum Color {
+		RED, BLACK
+	}
 }
